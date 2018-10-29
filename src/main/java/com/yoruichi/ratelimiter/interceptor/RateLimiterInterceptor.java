49f392d3eef426628e6db159c6932cc1480e7a85
wangguowen <wangguowen@superatomfin.com>
2018-10-29 17:40:12 +0800

    modify get ip

--- src/main/java/com/yoruichi/ratelimiter/interceptor/RateLimiterInterceptor.java
+++ src/main/java/com/yoruichi/ratelimiter/interceptor/RateLimiterInterceptor.java
@@ -1,5 +1,6 @@
 package com.yoruichi.ratelimiter.interceptor;
 
+import com.google.common.base.Strings;
 import com.yoruichi.ratelimiter.annotation.RateLimiterPolicies;
 import com.yoruichi.ratelimiter.annotation.RateLimiterPolicy;
 import com.yoruichi.ratelimiter.bean.RateLimiterPolicyBean;
@@ -126,41 +127,22 @@ public class RateLimiterInterceptor extends HandlerInterceptorAdapter implements
     }
 
     protected String getIPAddress(HttpServletRequest request) {
-        String ip = null;
+        String[] keys = new String[] {
+                //自定义ngx头
+                "real_remote_addr", "X-Forwarded-For",
+                //Proxy-Client-IP：apache 服务代理
+                "Proxy-Client-IP",
+                //WL-Proxy-Client-IP：weblogic 服务代理
+                "WL-Proxy-Client-IP",
+                //HTTP_CLIENT_IP：有些代理服务器
+                "HTTP_CLIENT_IP",
+                //X-Real-IP：nginx服务代理
+                "X-Real-IP" };
 
-        //X-Forwarded-For：Squid 服务代理
-        String ipAddresses = request.getHeader("X-Forwarded-For");
-
-        if (ipAddresses == null || ipAddresses.length() == 0 || "unknown".equalsIgnoreCase(ipAddresses)) {
-            //Proxy-Client-IP：apache 服务代理
-            ipAddresses = request.getHeader("Proxy-Client-IP");
-        }
-
-        if (ipAddresses == null || ipAddresses.length() == 0 || "unknown".equalsIgnoreCase(ipAddresses)) {
-            //WL-Proxy-Client-IP：weblogic 服务代理
-            ipAddresses = request.getHeader("WL-Proxy-Client-IP");
-        }
-
-        if (ipAddresses == null || ipAddresses.length() == 0 || "unknown".equalsIgnoreCase(ipAddresses)) {
-            //HTTP_CLIENT_IP：有些代理服务器
-            ipAddresses = request.getHeader("HTTP_CLIENT_IP");
-        }
-
-        if (ipAddresses == null || ipAddresses.length() == 0 || "unknown".equalsIgnoreCase(ipAddresses)) {
-            //X-Real-IP：nginx服务代理
-            ipAddresses = request.getHeader("X-Real-IP");
-        }
-
-        //有些网络通过多层代理，那么获取到的ip就会有多个，一般都是通过逗号（,）分割开来，并且第一个ip为客户端的真实IP
-        if (ipAddresses != null && ipAddresses.length() != 0) {
-            ip = ipAddresses.split(",")[0];
-        }
-
-        //还是不能获取到，最后再通过request.getRemoteAddr();获取
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ipAddresses)) {
-            ip = request.getRemoteAddr();
-        }
-        return ip;
+        String ipAddresses= null;
+        for (String k: keys){ if(Strings.isNullOrEmpty(ipAddresses) || "unknown".equalsIgnoreCase(ipAddresses)){ ipAddresses=request.getHeader(k);} else{ break;}}
+        String ip = (!Strings.isNullOrEmpty(ipAddresses) && !"unknown".equalsIgnoreCase(ipAddresses))?ipAddresses.split(",")[0]:null;
+        if (!Strings.isNullOrEmpty(ip) && !"unknown".equalsIgnoreCase(ip)){ return ip;} else{ return request.getRemoteAddr();}
     }
 
     /**
