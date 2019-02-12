c3cdacc1e775c2b73dcd6f7a89321919cbd2d318
wangguowen <wangguowen@superatomfin.com>
2019-02-12 14:41:26 +0800

    add auto scan

--- src/test/java/com/yoruichi/ratelimiter/TestController.java
+++ src/test/java/com/yoruichi/ratelimiter/TestController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @Author: Yoruichi
  * @Date: 2018/10/8 2:59 PM
