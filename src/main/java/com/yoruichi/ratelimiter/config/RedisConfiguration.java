3c99301ebd66b99a84bad800a38569dece69d97c
wangguowen <wangguowen@superatomfin.com>
2019-09-20 16:47:34 +0800

    remove redis

--- src/main/java/com/yoruichi/ratelimiter/config/RedisConfiguration.java
+++ src/main/java/com/yoruichi/ratelimiter/config/RedisConfiguration.java
@@ -1,22 +1,24 @@
 package com.yoruichi.ratelimiter.config;
 
-
-import com.google.common.base.Strings;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.data.redis.core.script.RedisScript;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.scripting.support.ResourceScriptSource;
-import redis.clients.jedis.JedisPoolConfig;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @author yoruichi
@@ -36,34 +38,38 @@ public class RedisConfiguration {
 
     @Bean(name = "rateLimiter")
     public RedisTemplate<String, List<Long>> redisTemplate(
-            @Value("${spring.redis.host:localhost}") String host,
-            @Value("${spring.redis.port:6379}") int port,
+            @Value("${spring.redis.host.rateLimiter:localhost}") String host,
+            @Value("${spring.redis.port.rateLimiter:6379}") int port,
             @Value("${spring.redis.database.rateLimiter:0}") int db,
-            @Value("${spring.redis.password:}") String password
+            @Value("${spring.redis.password.rateLimiter:}") String password,
+            @Value("${spring.redis.pool.max-active.rateLimiter:64}") int maxActive,
+            @Value("${spring.redis.pool.max-wait.rateLimiter:-1}") int maxWait,
+            @Value("${spring.redis.pool.max-idle.rateLimiter:64}") int maxIdle,
+            @Value("${spring.redis.pool.min-idle.rateLimiter:32}") int minIdle
     ) {
-        JedisConnectionFactory jedis = new JedisConnectionFactory();
-        jedis.setHostName(host);
-        jedis.setPort(port);
-        jedis.setDatabase(db);
-        if (!Strings.isNullOrEmpty(password)) {
-            jedis.setPassword(password);
+
+        GenericObjectPoolConfig poolConfig = new GenericObjectPoolConfig();
+        poolConfig.setMaxTotal(maxActive);
+        poolConfig.setMaxWaitMillis(maxWait);
+        poolConfig.setMaxIdle(maxIdle);
+        poolConfig.setMinIdle(minIdle);
+        poolConfig.setTestWhileIdle(true);
+        poolConfig.setMinEvictableIdleTimeMillis(60000L);
+        poolConfig.setTimeBetweenEvictionRunsMillis(30000L);
+        poolConfig.setNumTestsPerEvictionRun(-1);
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName(host);
+        configuration.setPort(port);
+        configuration.setDatabase(db);
+        if (Objects.nonNull(password) && password.trim().length() > 0) {
+            configuration.setPassword(password);
         }
 
-        JedisPoolConfig poolConfig = new JedisPoolConfig();
-        poolConfig.setMaxIdle(8);
-        poolConfig.setMaxWaitMillis(-1);
-        poolConfig.setTestOnBorrow(true);
-
-        jedis.setPoolConfig(poolConfig);
-        jedis.afterPropertiesSet();
-        RedisTemplate<String, List<Long>> template = new RedisTemplate();
-        template.setConnectionFactory(jedis);
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(configuration,
+                LettucePoolingClientConfiguration.builder().poolConfig(poolConfig).build());
+        RedisTemplate template = new StringRedisTemplate(lettuceConnectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        log.debug("Create redis template with url {}:{} pwd {}",
-                ((JedisConnectionFactory) template.getConnectionFactory()).getHostName(),
-                ((JedisConnectionFactory) template.getConnectionFactory()).getPort(),
-                ((JedisConnectionFactory) template.getConnectionFactory()).getPassword());
         return template;
     }
 
